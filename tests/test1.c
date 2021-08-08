a05681d5e578feaa18eec94873c5f432db0cae47
johpetsc <johpetsc@gmail.com>
2021-08-08 03:30:19 -0300

    Primeira versao do analisador lexico, ainda faltam algumas coisas.

---
+++ tests/test1.c
@@ -0,0 +1,18 @@
+int main(){ 
+    int a;
+    a = 5;
+    int list LI;
+    float list LF;
+
+    for(a){
+        write(a);
+        writeln("a");
+        read(a);
+    }
+    if(a){
+        a = a * a;
+    }else{
+        a = a + a;
+    }
+    return 0;
+}
